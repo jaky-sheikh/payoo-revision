20de5033920d9d3a48474304e4b602780add5139
jaky-sheikh <jaky@eu4m.eu>
2025-07-02 12:55:28 +0600

    bad way to validation

--- JS/login.js
+++ JS/login.js
@@ -2,7 +2,16 @@
 document.getElementById('btn-login').addEventListener('click', function (event) {
     // step-2: prevent reloading browser
     event.preventDefault();
-    //  step-3: get the phone number 
-    const phoneNumber = document.getElementById('phone-number');
-    console.log(phoneNumber.value);
+    //  step-3: get the phone number and the pin number
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+    // step-4: bad way to validate 
+    if (phoneNumber === '01917673432' && pinNumber === '1234') {
+        console.log('You are logged in')
+        window.location.href = '/home.html';
+    }
+    else {
+        alert('Wrong phone or pin number.')
+    }
 })
