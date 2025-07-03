aa143589e52f09b07e37fbc4299f956135501dd7
jaky-sheikh <jaky@eu4m.eu>
2025-07-03 10:29:45 +0600

    add money partial function done

--- JS/home.js
+++ JS/home.js
@@ -1,1 +1,12 @@
-console.log('home added');
+// step-1: add event handler to the btn 
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // step-2: get new amount 
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+    // step-3: check pin 
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber);
+})
