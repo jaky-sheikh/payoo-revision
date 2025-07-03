775bd68803abbb5d4b7c7c649c38ecaff038113e
jaky-sheikh <jaky@eu4m.eu>
2025-07-03 11:30:42 +0600

    add new balance with balance done

--- JS/home.js
+++ JS/home.js
@@ -6,7 +6,27 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
     const addMoneyInput = document.getElementById('input-add-money').value;
     console.log(addMoneyInput);
 
-    // step-3: check pin 
+    // step-3: get and verify pin(wrong way)
     const pinNumber = document.getElementById('pin-number').value;
     console.log(pinNumber);
+    if (pinNumber === '1234') {
+        console.log('Money added successfully');
+
+        // step-4: get the current balance 
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance);
+
+        // step-5: add addMoneyInput with balance 
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        // console.log(typeof newBalance);
+        console.log(newBalance);
+
+        // step-6: update the balance in the UI/DOM (set)
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else {
+        alert("Failed to add money. Please try again");
+    }
 })
